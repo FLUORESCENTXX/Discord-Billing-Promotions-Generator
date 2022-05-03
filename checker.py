12d975a5bdc27adba36c28e761232816243cc4ac
#Fluroescent <Zegitora@gmail.com>
2022-05-04 07:36:38 +1000

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 import requests
 
+# Theres no current working apis for checking these codes, this used to work if not try xbox game pass
 def get_promotion_codes(self):
     url = f"https://discord.com/api/v8/promotions/pics-art"
     headers = {"authorization: self.token,
